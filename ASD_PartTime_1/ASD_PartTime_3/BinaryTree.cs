31e5d605aa88d2994228745655b1a6c024222914
Hasan Gyulyustan <hasan_17@abv.bg>
2019-03-26 11:10:32 +0000

    Binary tree added

---
+++ ASD_PartTime_1/ASD_PartTime_3/BinaryTree.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD_PartTime_3
+{
+    public class BinaryTreeNode<T>
+
+    {
+
+        // Contains the value of the node
+
+        private T value;
+
+
+
+        // Shows whether the current node has parent
+
+        private bool hasParent;
+
+
+
+        // Contains the left child of the node
+
+        private BinaryTreeNode<T> leftChild;
+
+
+
+        // Contains the right child of the node
+
+        private BinaryTreeNode<T> rightChild;
+
+
+
+        /// <summary>
+
+        /// Constructs a binary tree node
+
+        /// </summary>
+
+        /// <param name="value">the value of the node</param>
+
+        /// <param name="leftChild">the left child of the node</param>
+
+        /// <param name="rightChild">the right child of the
+
+        /// node</param>
+
+        public BinaryTreeNode(T value,
+
+                    BinaryTreeNode<T> leftChild,
+
+                    BinaryTreeNode<T> rightChild)
+
+        {
+
+            if (value == null)
+
+            {
+
+                throw new ArgumentNullException(
+
+                      "Cannot insert null value!");
+
+            }
+
+
+
+            this.value = value;
+
+            this.LeftChild = leftChild;
+
+            this.RightChild = rightChild;
+
+        }
+
+
+
+        /// <summary>
+
+        /// Constructs a binary tree node with no children
+
+        /// </summary>
+
+        /// <param name="value">the value of the node</param>
+
+        public BinaryTreeNode(T value) : this(value, null, null)
+
+        {
+
+        }
+
+
+
+        /// <summary>
+
+        /// The value of the node
+
+        /// </summary>
+
+        public T Value
+
+        {
+
+            get
+
+            {
+
+                return this.value;
+
+            }
+
+            set
+
+            {
+
+                this.value = value;
+
+            }
+
+        }
+
+
+
+        /// <summary>
+
+        /// The left child of the node
+
+        /// </summary>
+
+        public BinaryTreeNode<T> LeftChild
+
+        {
+
+            get
+
+            {
+
+                return this.leftChild;
+
+            }
+
+            set
+
+            {
+
+                if (value == null)
+
+                {
+
+                    return;
+
+                }
+
+
+
+                if (value.hasParent)
+
+                {
+
+                    throw new ArgumentException(
+
+                          "The node already has a parent!");
+
+                }
+
+
+
+                value.hasParent = true;
+
+                this.leftChild = value;
+
+            }
+
+        }
+
+
+
+        /// <summary>
+
+        /// The right child of the node
+
+        /// </summary>
+
+        public BinaryTreeNode<T> RightChild
+
+        {
+
+            get
+
+            {
+
+                return this.rightChild;
+
+            }
+
+            set
+
+            {
+
+                if (value == null)
+
+                {
+
+                    return;
+
+                }
+
+
+
+                if (value.hasParent)
+
+                {
+
+                    throw new ArgumentException(
+
+                          "The node already has a parent!");
+
+                }
+
+
+
+                value.hasParent = true;
+
+                this.rightChild = value;
+
+            }
+
+        }
+
+    }
+
+
+
+    /// <summary>
+
+    /// Represents a binary tree structure
+
+    /// </summary>
+
+    /// <typeparam name="T">the type of the values in the
+
+    /// tree</typeparam>
+
+    public class BinaryTree<T>
+
+    {
+
+        // The root of the tree
+
+        private BinaryTreeNode<T> root;
+
+
+
+        /// <summary>
+
+        /// Constructs the tree
+
+        /// </summary>
+
+        /// <param name="value">the value of the root node</param>
+
+        /// <param name="leftChild">the left child of the root node</param>
+
+        /// <param name="rightChild">the right child of the
+
+        /// root node</param>
+
+        public BinaryTree(T value, BinaryTree<T> leftChild,
+
+                    BinaryTree<T> rightChild)
+
+        {
+
+            if (value == null)
+
+            {
+
+                throw new ArgumentNullException(
+
+                      "Cannot insert null value!");
+
+            }
+
+
+
+            BinaryTreeNode<T> leftChildNode =
+
+                  leftChild != null ? leftChild.root : null;
+
+
+
+            BinaryTreeNode<T> rightChildNode =
+
+                  rightChild != null ? rightChild.root : null;
+
+
+
+            this.root = new BinaryTreeNode<T>(
+
+                  value, leftChildNode, rightChildNode);
+
+        }
+
+
+
+        /// <summary>
+
+        /// Constructs the tree
+
+        /// </summary>
+
+        /// <param name="value">the value of the root node</param>
+
+        public BinaryTree(T value)
+
+              : this(value, null, null)
+
+        {
+
+        }
+
+
+
+        /// <summary>
+
+        /// The root of the tree.
+
+        /// </summary>
+
+        public BinaryTreeNode<T> Root
+
+        {
+
+            get
+
+            {
+
+                return this.root;
+
+            }
+
+        }
+
+
+
+        /// <summary>
+
+        /// Traverses binary tree in pre-order manner
+
+        /// </summary>
+
+        /// <param name="root">the binary tree to be traversed</param>
+
+        private void PrintInorder(BinaryTreeNode<T> root)
+
+        {
+
+            if (root == null)
+
+            {
+
+                return;
+
+            }
+
+
+
+            // 1. Visit the left child
+
+            PrintInorder(root.LeftChild);
+
+
+
+            // 2. Visit the root of this subtree
+
+            Console.Write(root.Value + " ");
+
+
+
+            // 3. Visit the right child
+
+            PrintInorder(root.RightChild);
+
+        }
+
+
+
+        /// <summary>
+
+        /// Traverses and prints the binary
+
+        /// tree in pre-order manner
+
+        /// </summary>
+
+        public void PrintInorder()
+
+        {
+
+            PrintInorder(this.root);
+
+            Console.WriteLine();
+
+        }
+
+    }
+}
--- ASD_PartTime_1/ASD_PartTime_3/Program.cs
+++ ASD_PartTime_1/ASD_PartTime_3/Program.cs
@@ -42,31 +42,66 @@ namespace ASD_PartTime_3
              */
 
 
-            
-             //Graph example
-             Graph g = new Graph(new int[][] {
-			new int[] {3, 6}, // successors of vertice 0
-			new int[] {2, 3, 4, 5, 6}, // successors of vertice 1
-			new int[] {1, 4, 5}, // successors of vertice 2
-			new int[] {0, 1, 5}, // successors of vertice 3
-			new int[] {1, 2, 6}, // successors of vertice 4
-			new int[] {1, 2, 3}, // successors of vertice 5
-			new int[] {0, 1, 4}  // successors of vertice 6
-		});
+            /*
+               //Graph example
+               Graph g = new Graph(new int[][] {
+              new int[] {3, 6}, // successors of vertice 0
+              new int[] {2, 3, 4, 5, 6}, // successors of vertice 1
+              new int[] {1, 4, 5}, // successors of vertice 2
+              new int[] {0, 1, 5}, // successors of vertice 3
+              new int[] {1, 2, 6}, // successors of vertice 4
+              new int[] {1, 2, 3}, // successors of vertice 5
+              new int[] {0, 1, 4}  // successors of vertice 6
+          });
 
-		Console.Write("Breadth-First Search (BFS) traversal: ");
-		g.TraverseBFS(0);
-		Console.WriteLine();
+          Console.Write("Breadth-First Search (BFS) traversal: ");
+          g.TraverseBFS(0);
+          Console.WriteLine();
 
-		Console.Write("Depth-First Search (DFS) traversal (with stack): ");
-		g.TraverseDFS(0);
-		Console.WriteLine();
+          Console.Write("Depth-First Search (DFS) traversal (with stack): ");
+          g.TraverseDFS(0);
+          Console.WriteLine();
 
-		Console.Write("Depth-First Search (DFS) traversal (recursive): ");
-		bool[] visited = new bool[Graph.MaxNode];
-		g.TraverseDFSRecursive(0, visited);
-		Console.WriteLine();
-             
+          Console.Write("Depth-First Search (DFS) traversal (recursive): ");
+          bool[] visited = new bool[Graph.MaxNode];
+          g.TraverseDFSRecursive(0, visited);
+          Console.WriteLine();
+           */
+
+
+            // Create the binary tree from the sample
+
+            BinaryTree<int> binaryTree =
+
+                  new BinaryTree<int>(14,
+
+                              new BinaryTree<int>(19,
+
+                                          new BinaryTree<int>(23),
+
+                                          new BinaryTree<int>(6,
+
+                                                      new BinaryTree<int>(10),
+
+                                                      new BinaryTree<int>(21))),
+
+                              new BinaryTree<int>(15,
+
+                                          new BinaryTree<int>(3),
+
+                                          null));
+
+
+
+            // Traverse and print the tree in in-order manner
+
+            binaryTree.PrintInorder();
+
+
+
+            // Console output:
+
+            // 23 19 10 6 21 14 3 15
 
         }
     }
